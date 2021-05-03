c92d1891a89710237246cba272c2703dd41901bd
Satoru Tanaka <ittimfn@gmail.com>
2021-05-03 21:49:06 +0900

    fix #1 形態素解析結果との比較処理作成

--- app/app.py
+++ app/app.py
@@ -33,12 +33,12 @@ DIC_PATH = ' -d /usr/local/mecab/lib/mecab/dic/mecab-ipadic-neologd'
 def read_comment_json(input_file):
     """ コメントjsonを読み込む
     """
-    return_dict = {}
+    return_dict = []
 
     with open(INPUT_FILE, mode='r') as f:
         comments = json.load(f)
         for comment in comments:
-            return_dict[comment[CHAT_KEYS[DICT_CHAT_KEY]]] = comment
+            return_dict.append(comment)
 
     return return_dict
 
@@ -46,18 +46,19 @@ def read_ng_pattern():
     """ NGパターンの形態素解析結果を読み込んで返す
     """
     ng_pattern = {}
-    for file in [p for p in glob.glob(NG_PATTERN_DIR , recursive=True) if os.path.isfile(p) and os.path.splitext(p)[1][1:] == 'gitkeep' ]:
+    for file in [p for p in glob.glob(NG_PATTERN_DIR , recursive=True) if os.path.isfile(p) and os.path.splitext(p)[1][1:] != 'gitkeep' ]:
         with open(file) as f:
-            ng_pattern[file] = f
+            ng_pattern[file] = f.read()
+            # print(ng_pattern[file])
     return ng_pattern
 
 
-def get_ng_chat_id(comment_dict, ng_pattern, threshold):
+def get_ng_chat_id(comment_list, ng_pattern, threshold):
     """ NGパターンに引っかかったコメントIDと、どのパターンに引っかかったかを返す。
     """
-    ng_comments = []
-    for comment_key in comment_dict:
-        mplg_result = mplg(comment_dict[comment_key][CHAT_KEYS[DICT_CHAT_TEXT]])
+    ng_comments = {}
+    for comment_key in range(0, len(comment_list)):
+        mplg_result = mplg(comment_list[comment_key][CHAT_KEYS[DICT_CHAT_TEXT]])
         for ng_key in ng_pattern:
             similarity = SequenceMatcher(
                 None,mplg_result,ng_pattern[ng_key]).ratio()
@@ -66,24 +67,25 @@ def get_ng_chat_id(comment_dict, ng_pattern, threshold):
                     NG_RESULT_KEYS[INDEX_NG_RESULT_PATTERN]: ng_key,
                     NG_RESULT_KEYS[INDEX_NG_RESULT_SIMILARITY]: similarity
                 }
+                break
     return ng_comments
 
-def merge_ng_comments(comments, ng_comments):
+def merge_ng_comments(comment_list, ng_comment_dict):
     """ コメントにNG情報を付与する
     """
     
-    return_comments = comments
-    
-    for key in comments:
-        ng_info = {}
-        if key in ng_comments:
-            return_comments[key]['ng_flg'] = True
-            ng_info = ng_comments[key]
-        else:
-            return_comments[key]['ng_flg'] = False
+    return_comment_list = []
 
-        return_comments[key]['ng_info'] = ng_info
-    return return_comments
+    for key in range(0, len(comment_list)):
+        comment_info = comment_list[key]
+        if key in ng_comment_dict:
+            comment_info[NG_FLG_KEY] = True
+            comment_info[NG_INFO_KEY] = ng_comment_dict[key]
+        else:
+            comment_info[NG_FLG_KEY] = False
+
+        return_comment_list.append(comment_info)
+    return return_comment_list
 
 def mplg(text):
     """ 形態素解析を行う。
@@ -103,13 +105,18 @@ if __name__ == '__main__':
     # NGパターンを読み込む
     ng_pattern = read_ng_pattern()
 
-    # コメントファイルをdictに変換する。
-    item_dict = read_comment_json(INPUT_FILE)
+    # コメントファイルを配列に変換する。
+    comment_list = read_comment_json(INPUT_FILE)
+
+#    with open('./tmp/test.json' , mode='w') as f:
+#        f.write(json.dumps(comment_infos))
 
     # NGェック
-    ng_comments = get_ng_chat_id(item_dict, ng_pattern, 0.8)
+    ng_comment_dict = get_ng_chat_id(comment_list, ng_pattern, 0.8)
 
     # コメントにNGフラグ、引っかかったパターン、類似度を付与する。
-    comments_ng_merged = merge_ng_comments(item_dict, ng_comments)    
+    comments_ng_merged = merge_ng_comments(comment_list, ng_comment_dict)
     
-    print(json.dumps(comments_ng_merged))
+    # 結果をファイルに出力
+    with open(OUTPUT_DIR + '/' + 'result_' + VIDEO_ID + '.json' , mode='w') as f:
+        f.write(json.dumps(comments_ng_merged))
--- start.sh
+++ start.sh
@@ -3,7 +3,3 @@
 INPUT_FILE=/app/input/${VIDEO_ID}.json
 python app.py ${VIDEO_ID} ${INPUT_FILE} 
 
-# export PATH=/usr/local/mecab/bin:$PATH
-# mecab input/comments.txt -o tmp/result.txt
-# csplit -z -f output/result -n 5 tmp/result.txt /EOS/ {*} 1> /dev/null
-
