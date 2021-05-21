ec6ce4f9a911a65bf28ac5dfba2afa3dc911bb75
SampleUser0001 <ittimfn@gmail.com>
2021-05-21 18:11:08 +0900

    Feature/issue 4 (#13)
    * チャンネルURLを出力するように修正
    * ユーザID一覧を出力するように修正
    * 出力ファイルパスについて修正
    * コメントの出力ファイル名を修正。

--- app/app.py
+++ app/app.py
@@ -8,8 +8,9 @@ from difflib import SequenceMatcher , Differ
 
 NG_PATTERN_DIR = './input/ng_pattern/**'
 
-OUTPUT_DIR = './output/'
-FILE_NAME = 'result'
+OUTPUT_COMMENT_DIR = './output/comment/'
+OUTPUT_CHANNEL_DIR = './output/channel/'
+OUTPUT_USER_DIR = './output/user/'
 
 # jsonファイルの読み込み
 CHAT_KEYS = ["authorExternalChannelId","user","timestampUsec","time","authorbadge","text","purchaseAmount","type","video_id","Chat_No"]
@@ -29,6 +30,9 @@ NG_INFO_KEY = 'ng_info'
 
 # 辞書ファイルパス
 DIC_PATH = ' -d /usr/local/mecab/lib/mecab/dic/mecab-ipadic-neologd'
+
+# YoutubeチャンネルURL
+YOUTUBE_CHANNEL = 'https://www.youtube.com/channel/'
 
 def read_comment_json(input_file):
     """ コメントjsonを読み込む
@@ -53,10 +57,35 @@ def read_ng_pattern():
     return ng_pattern
 
 
-def get_ng_chat_id(comment_list, ng_pattern, threshold):
+def get_ng(comment_list, ng_pattern, threshold):
     """ NGパターンに引っかかったコメントIDと、どのパターンに引っかかったかを返す。
+    Parameters:
+    ----
+    comment_list : jsonの配列
+        読み込んだコメントの配列。
+        read_comment_json(string)の戻り値。
+    ng_pattern : dict
+        読み込んだNGパターンの形態素解析結果の辞書。
+        read_ng_pattern()の戻り値。
+    threshold : float
+        類似度のしきい値。
+    
+    Returns:
+    ----
+    ng_comments: dict
+        NG判定したコメントの辞書。
+        key : コメントID
+        value : json
+            pattern : 一致した形態素解析結果ファイル
+            similarity : 類似度
+    ng_channel_list : list
+        NG判定したチャンネルURL
+    ng_user_list : list
+        NG判定したユーザID
     """
     ng_comments = {}
+    ng_channel_list = []
+    ng_user_list = []
     for comment_key in range(0, len(comment_list)):
         mplg_result = mplg(comment_list[comment_key][CHAT_KEYS[DICT_CHAT_TEXT]])
         for ng_key in ng_pattern:
@@ -67,8 +96,14 @@ def get_ng_chat_id(comment_list, ng_pattern, threshold):
                     NG_RESULT_KEYS[INDEX_NG_RESULT_PATTERN]: ng_key,
                     NG_RESULT_KEYS[INDEX_NG_RESULT_SIMILARITY]: similarity
                 }
+                
+                # ユーザの取得
+                ng_user = comment_list[comment_key][CHAT_KEYS[DICT_CHAT_CHANNNELID]]
+                if ng_user not in ng_user_list:
+                    ng_user_list.append(ng_user)
+                    ng_channel_list.append(YOUTUBE_CHANNEL + ng_user)
                 break
-    return ng_comments
+    return ng_comments, ng_channel_list, ng_user_list
 
 def merge_ng_comments(comment_list, ng_comment_dict):
     """ コメントにNG情報を付与する
@@ -94,7 +129,6 @@ def mplg(text):
     soup = m.parse (text)
     return soup
 
-
 if __name__ == '__main__':
     args_index = 0
     args_index = args_index + 1
@@ -112,11 +146,20 @@ if __name__ == '__main__':
 #        f.write(json.dumps(comment_infos))
 
     # NGェック
-    ng_comment_dict = get_ng_chat_id(comment_list, ng_pattern, 0.8)
+    ng_comment_dict, ng_channel_list, ng_user_list = get_ng(comment_list, ng_pattern, 0.8)
 
     # コメントにNGフラグ、引っかかったパターン、類似度を付与する。
     comments_ng_merged = merge_ng_comments(comment_list, ng_comment_dict)
-    
-    # 結果をファイルに出力
-    with open(OUTPUT_DIR + 'result_' + VIDEO_ID + '.json' , mode='w') as f:
+    # NGフラグを付与したコメントを出力する。
+    with open(OUTPUT_COMMENT_DIR + VIDEO_ID + '.json' , mode='w') as f:
         f.write(json.dumps(comments_ng_merged))
+
+    # print(ng_channel_list)
+    # NG判定したチャンネル一覧を出力する
+    with open(OUTPUT_CHANNEL_DIR + VIDEO_ID + '.txt' , mode='w') as f:
+        f.write('\n'.join(ng_channel_list))
+        # newlineオプション + writelines関数が聞かない？
+        
+    # NG判定したユーザ一覧を出力する
+    with open(OUTPUT_USER_DIR + VIDEO_ID + '.txt' , mode='w') as f:
+        f.write('\n'.join(ng_user_list))
